84bc6ecea16a5ebfb2c07a7c9bda5d4e17499d26
Amy Dalziel <amy.dalziel@keyin.com>
2024-08-01 20:17:15 -0230

    Slideshow Images working

--- src/App.js
+++ src/App.js
@@ -13,46 +13,54 @@ import Footer from './components/Footer';
 
 function App() {
 
-  const [color, setColor] = useState(null);
-  const [loading, setLoading] = useState(true);
+  // const [color, setColor] = useState(null);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchColor = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/color/1');
-        setColor(response.data);
-      } catch (error) {
-        console.error("Error: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchColor = async () => {
+  //     try {
+  //       const response = await axios.get('http://localhost:8080/color/1');
+  //       setColor(response.data);
+  //     } catch (error) {
+  //       console.error("Error: ", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchColor();
-  }, []);
+  //   fetchColor();
+  // }, []);
 
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+const mainSlideshowImages = [
+  '../images/yellow-slideshow.jpg',
+  '../images/blue-slideshow.jpg',
+  '../images/red-slideshow.jpg',
+  '../images/green-slideshow.jpg',
+  '../images/pink-slideshow.jpg',
+  '../images/brown-slideshow.jpg'
+]; 
 
 
   return (
     <div className="App">
       <NavBar />
-      <MainSlideshow />
+      <MainSlideshow 
+      images={mainSlideshowImages}
+      duration = '5000'
+       />
       <UserChoices /> 
       <InspirationSlideshow /> 
       <Footer /> 
-    <header classN
+    {/* <header classN
     ame="App-header">
     <h1>{color.name}</h1>
         <p>ID: {color.id}</p>
         <p>Hex Number: {color.hexNumber}</p>
         {/* <p>Vibe List: {color.vibeList}</p>  */}
-        <p>Space: {color.space.spaceName}</p>
-        <p>Style: {color.style.styleName}</p>
-    </header>
+        {/* <p>Space: {color.space.spaceName}</p>
+        <p>Style: {color.style.styleName}</p> */}
+    {/* </header> */} */
   </div>
 );
 }
