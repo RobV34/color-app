5336af9abcd80610ea73ca21ba88fbd39cefce94
Amy Dalziel <amy.dalziel@keyin.com>
2024-08-03 21:54:44 -0230

    User choice options expanded

--- src/components/UserChosenColor.jsx
+++ src/components/UserChosenColor.jsx
@@ -11,7 +11,6 @@ const UserChosenColor = () => {
         return <p>No color details available.</p>;
       } 
 
-
       function hexToRgb(hex) {
         console.log(hex); 
         hex = hex.replace('#', '');
@@ -32,11 +31,41 @@ const UserChosenColor = () => {
         return luminance > 0.5 ? 'black' : 'white';
     }
 
+    function chooseSpaceFontColor(hexColor) {
+      let [r, g, b] = hexToRgb(hexColor);
+      let luminance = getLuminance(r, g, b);
+      return luminance > 0.5 ? 'rgb(36, 36, 36)' : 'rgb(222, 219, 219)';
+  }
+
+    const vibeNames = color.vibeList.map(vibe => vibe.vibeName.toUpperCase()).join(' • ');
+
+
+    let userSpaceMessage = ""; 
+
+    switch (color.space.id) {
+      case 1: 
+        userSpaceMessage = "Check out office supplies on sale: "; 
+        break;
+      case 2: 
+        userSpaceMessage = "Check out top trending kitchen gadgets: "; 
+        break; 
+      case 3: 
+        userSpaceMessage = "Must-haves for your new bathroom: "; 
+        break; 
+      default:
+        break;
+    }
+
   return (
     <div id='UserChosenColor-Box' style={{backgroundColor: color.hexNumber, color: chooseFontColor(color.hexNumber)}}>
       <h3>Your perfect match ...</h3>
       <h1>{color.name}</h1>
       <h2>Hex Number: {color.hexNumber}</h2>
+      
+      <h3> {vibeNames} </h3>
+
+      <h2 style={{color: chooseSpaceFontColor(color.hexNumber), border: `2px solid ${chooseSpaceFontColor(color.hexNumber)}`, padding: '10px', borderRadius: '20px', maxWidth: '80%' }}>{userSpaceMessage}</h2>
+ 
     </div>
   );
 };
